6e923e8ed5363233070fc89b2bd4f04067429706
Rafael Carvalho J. Araujo <rrafaelcarvalhoj@gmail.com>
2023-12-26 23:22:10 -0300

    fix: server api connected to database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const bodyparser = require('body-parser');
 const sequelize = require('./util/database');
 const Exercice  = require('./models/exercice');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
--- util/database.js
+++ util/database.js
@@ -1,13 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    'postgres',
-    'postgres',
-    '12345678',
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-    }
-);
+const sequelize = new Sequelize(`postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:5432/${process.env.PG_DB}`);
 
 module.exports = sequelize;
